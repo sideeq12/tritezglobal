16be40985c72df846eea92a3f1c4104714190a66
sideeq12 <abdwaheed2018@gmail.com>
2022-05-22 13:21:47 +0000

    home layout

--- components/mission/mission.tsx
+++ components/mission/mission.tsx
@@ -1,8 +1,24 @@
 import {Mission_style} from "./mission_style"
+import { useEffect } from "react"
+import { gsap, Power4 } from "gsap"
 
 export const Mission = () =>{
+    useEffect(()=>{
+        let tl = gsap.timeline({
+            scrollTrigger: {
+              trigger: "p",
+              start: "60%",
+              end: "80%",
+              scrub: 1
+            }
+          });
+        tl.to("h2, p", { "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)", 
+         opacity : 1,
+         y : 0,
+         duration : 0.5})
+    }, [])
     return (
-        <Mission_style>
+        <Mission_style className="mission">
             <h2>Our Speciality</h2>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur, 
                 adipisci error. Perspiciatis provident nulla accusantium officiis a possimus 
