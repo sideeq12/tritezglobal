668450a5cd3a06a0187e64891f37c5d1feecf700
sideeq12 <abdwaheed2018@gmail.com>
2022-05-23 12:21:51 +0000

    gsap url fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,9 +7,6 @@ import Head from 'next/head'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Head>
-    {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js" integrity="sha512-VEBjfxWUOyzl0bAwh4gdLEaQyDYPvLrZql3pw1ifgb6fhEvZl9iDDehwHZ+dsMzA0Jfww8Xt7COSZuJ/slxc4Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> */}
-    </Head>
     <Navbar />
     <Component {...pageProps} />
     <Footer />
