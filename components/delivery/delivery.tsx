c28fb9750a8f993c866996ab2818f45db3772f31
sideeq12 <abdwaheed2018@gmail.com>
2022-06-09 20:08:35 +0000

    sign in fix

--- components/delivery/delivery.tsx
+++ components/delivery/delivery.tsx
@@ -30,7 +30,7 @@ export  const Delivery = ({image, title, message , check} : Props)=>{
          "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)", 
     opacity : 1,
     y : 0,
-    duration : 0.5, delay : 0.2})
+    duration : 0.5, delay : 0.1})
     }, [])
    
     return(
