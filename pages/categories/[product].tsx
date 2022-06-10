34ed44702aea1069e925fa6977e3f2b81473faf3
sideeq12 <abdwaheed2018@gmail.com>
2022-06-10 06:24:39 +0000

    all user

--- pages/categories/[product].tsx
+++ pages/categories/[product].tsx
@@ -6,19 +6,28 @@ import { Card } from '../../components/card/card';
 import { useState } from 'react';
 import { My_link, All } from '../../styles/user_p';
 import Link from "next/link"
+import { useEffect } from 'react';
 
 const Unique_category= (numberkey: number) => {
   const router = useRouter()
   let par : string;
   const { product } = router.query;
+ 
 
   const [showFilter, setShowFilter ] = useState(true)
+  console.log(database.filter(word => word.category.includes("accessories")))
   const result = database.filter( word => word.category.includes("Blouse"))
   const rndInt = Math.floor(Math.random() * 6) + 1;
   const image1 = result[rndInt].image_link
   const rndInt2 = Math.floor(Math.random() * 6) + 1;
   const image2 = result[rndInt2].image_link
-  console.log( "the result is ", typeof(product))
+
+    useEffect(()=>{
+        console.log("the product is ", router.query)
+    }, [product])
+
+
+
   return (<All>
       <My_link>
       <Link href="/">Home </Link>
