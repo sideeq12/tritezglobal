10e641becc4d07423eaabe5f79580971683c3503
sideeq12 <abdwaheed2018@gmail.com>
2022-06-17 16:15:13 +0000

    set up API

--- pages/api/clothes.ts
+++ pages/api/clothes.ts
@@ -1,6 +1,9 @@
+import { NextApiRequest, NextApiResponse } from "next";
 
+type Data = {
+    users :string
+}
 
-
-export const DB = {
-    hello : "testing"
+export default function getUsers(req : NextApiRequest, res : NextApiResponse<Data>){
+    res.status(200).json({ users : "hello test"})
 }
