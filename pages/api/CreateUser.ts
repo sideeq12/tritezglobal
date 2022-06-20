e62dd5a1657eb8a7165e5bcbf618efac7cc30a7e
sideeq12 <abdwaheed2018@gmail.com>
2022-06-20 21:08:04 +0000

    email used

--- pages/api/CreateUser.ts
+++ pages/api/CreateUser.ts
@@ -10,22 +10,31 @@ import bcrypt from "bcryptjs"
 export default function getUsers(req : NextApiRequest, res : NextApiResponse){
     let params = req.body;
     main().catch(error => console.log(error));
-    const create = new User({
-        full_name : params.userfullname,
-        password : bcrypt.hashSync(params.userPassword),
-        imageUrl : "imageUrl",
-        email : params.userEmail,
-        myOrders : [{
-            id : 2,
-            address : "oriire area shaki",
-            order : [{
-                id : 3,
-                name : "Latest Clothes",
-                price : 34
-            }]
-        }]
+    const user = User.findOne({ email : params.userEmail}, (err : Error, result : any )=>{
+        if(!err && result){
+            res.status(200).json({ message : "Email already used!"})
+        }else if(!err &&!result){
+            const create = new User({
+                full_name : params.userfullname,
+                password : bcrypt.hashSync(params.userPassword),
+                imageUrl : "imageUrl",
+                email : params.userEmail,
+                myOrders : [{
+                    id : 2,
+                    address : "oriire area shaki",
+                    order : [{
+                        id : 3,
+                        name : "Latest Clothes",
+                        price : 34
+                    }]
+                }]
+            })
+            create.save().then(()=> {
+                res.status(200).json({ message : "success", data : create})
+            })
+        }
     })
-    create.save().then(()=> {
-        res.status(200).json({ message : "success", data : create})
-    })
+        
+        
 }
+
